49ebe77e76031a827859a7d887373eb9567d2a7a
om bante <ombante56.in>
2026-02-22 15:35:12 +0530

    data ingestion

--- src/Components/data_ingestion.py
+++ src/Components/data_ingestion.py
@@ -9,43 +9,43 @@ from dataclasses import dataclass
 
 @dataclass
 class DataIngestionConfig:
-    tran_data_path: str = os.path.join('artifacts', "train.csv")
+    train_data_path: str = os.path.join('artifacts', "train.csv")
     test_data_path: str = os.path.join('artifacts', "test.csv")
-    raw_data_path: str = os.path.join('artifacts', "raw.csv")
+    raw_data_path: str = os.path.join('artifacts', "data.csv")
 
 class DataIngestion:
     def __init__(self):
         self.ingestionconfig = DataIngestionConfig()
 
-        def initiate_data_ingestion(self):
-            logging.info("Entered the data ingestion method or component")
-            try:
-                df = pd.read_csv('mlproject/notebook/data/stud.csv')
-                logging.info("Read the dataset as dataframe")
+    def initiate_data_ingestion(self):
+        logging.info("Entered the data ingestion method or component")
+        try:
+            df = pd.read_csv('notebook/data/stud.csv')
+            logging.info("Read the dataset as dataframe")
 
-                os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+            os.makedirs(os.path.dirname(self.ingestionconfig.train_data_path),exist_ok=True)
 
-                df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
+            df.to_csv(self.ingestionconfig.raw_data_path, index=False, header=True)
 
-                logging.info("Train test split initiated")
-                train_test,test_set = train_test_split(df, test_size=0.2, random_state=42)
+            logging.info("Train test split initiated")
+            train_set,test_set = train_test_split(df, test_size=0.2, random_state=42)
 
-                train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
+            train_set.to_csv(self.ingestionconfig.train_data_path, index=False, header=True)
 
-                test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
+            test_set.to_csv(self.ingestionconfig.test_data_path, index=False, header=True)
 
-                logging.info("Ingestion of the data is completed")
+            logging.info("Ingestion of the data is completed")
 
-                return(
-                    self.ingestion_config.train_data_path,
-                    self.ingestion_config.test_data_path
+            return(
+                self.ingestionconfig.train_data_path,
+                self.ingestionconfig.test_data_path
+            )
+        except Exception as e:
+            raise CustomException(e, sys)
 
 
-                )
-            except Exception as e:
-                raise CustomException(e, sys)
-            if __name__ == "__main__":
-                obj = DataIngestion()
-                obj.initiate_data_ingestion()
+if __name__ == "__main__":
+    obj = DataIngestion()
+    obj.initiate_data_ingestion()
     
           
